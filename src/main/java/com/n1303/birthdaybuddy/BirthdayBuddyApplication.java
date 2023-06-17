ce5623355d5b9417f4f707ccf9343ee4dc96d3fd
ShervilG <wintergpt45@gmail.com>
2023-06-17 22:39:11 +0530

    First commit to master

---
+++ src/main/java/com/n1303/birthdaybuddy/BirthdayBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.n1303.birthdaybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/n1303/birthdaybuddy/BirthdayBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n1303.birthdaybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

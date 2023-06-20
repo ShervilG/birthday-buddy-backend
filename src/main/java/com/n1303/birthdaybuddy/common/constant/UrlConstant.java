5c284d263214d1ac9c3430fbb45d2de3e8667ee9
Shervil Gupta <35729671+ShervilG@users.noreply.github.com>
2023-06-20 23:34:14 +0530

    Added common packages and Test Controller (#3)

---
+++ src/main/java/com/n1303/birthdaybuddy/common/constant/UrlConstant.java
@@ -0,0 +1,10 @@
+package com.n1303.birthdaybuddy.common.constant;
+
+public class UrlConstant {
+  // TestController
+  public static final String TEST_API_BASE_URL = "/test";
+  public static final String PING_API_URL = "/ping";
+
+  // Frontend
+  public static final String FRONTEND_APP_BASE_URL = "http://localhost:4200";
+}
